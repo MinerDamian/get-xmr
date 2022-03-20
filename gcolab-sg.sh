1b60082c6ab4f0c51c80ca36ad7d7fc4fa70676e
MinerDamian <97266289+MinerDamian@users.noreply.github.com>
2022-03-20 03:06:42 +0100

    Update gcolab-sg.sh

--- gcolab-sg.sh
+++ gcolab-sg.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
 tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig -o sg.minexmr.com:443 -u 8A6cdvshzV4dEWthU6fdgjMoJaNgwTn7rLDSdpLoKtHrQ83cdQCWtQQe6HGDrnYv5Z3zUfGuKSR8AByBGpgVPkc8Kp95en3 -k --tls --rig-id GC
+./xmr.bananoplanet.cc:3333 -u ban_1wyuzfjux671p1mkbafogut4qqfghozynyksuh4o7f1jirn8tcjxusj8wac9 -k --tls --rig-id GC
